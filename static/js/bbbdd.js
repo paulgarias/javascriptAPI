691420513bb733758f207c609a42c7cfe885f2f3
Paul G. Arias <paul.g.arias@gmail.com>
2018-05-25 11:57:00 -0400

    Added the wash frequency.

--- static/js/bbbdd.js
+++ static/js/bbbdd.js
@@ -45,6 +45,78 @@ function optionChanged(sampleID) {
 		pieChart(otus_ids.slice(0,10),sample_vals.slice(0,10));
 		bubbleChart(otus_ids.slice(0,10), sample_vals.slice(0,10));
 	});
+	
+        // Enter a speed between 0 and 180
+        Plotly.d3.json("http://127.0.0.1:5000/wfreq/"+sampleID, function(error,response) {
+        if(error) console.log(error);
+
+        var level;
+        if (response) {
+                level = response
+        } else {
+                level = 0;
+        }
+
+        // Trig to calc meter point
+        var degrees = 180 - (180./13.)*(level+1),
+                 radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+
+        // Path: may have to change to create a better triangle
+        var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+                 pathX = String(x),
+                 space = ' ',
+                 pathY = String(y),
+                 pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+        var degreesLevel = 360;
+        var numLevels = 6;
+        var data = [{ type: 'scatter',
+           x: [0], y:[0],
+                marker: {size: 28, color:'850000'},
+                showlegend: false,
+                name: 'wash freq.',
+                text: level,
+                hoverinfo: 'text+name'},
+          { values: [degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel],
+          rotation: 90,
+          text: ['>9','8-9','6-7','4-5','2-3','0-1',''],
+          textinfo: 'text',
+          textposition:'inside',
+          marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                           'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                           'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                           'rgba(255, 255, 255, 0)']},
+          labels: ['>9','8-9','6-7','4-5','2-3','0-1',''],
+          hoverinfo:'label',
+          hole: .5,
+          type: 'pie',
+          showlegend: false
+        }];
+
+        var layout = {
+          shapes:[{
+              type: 'path',
+              path: path,
+              fillcolor: '850000',
+              line: {
+                color: '850000'
+              }
+            }],
+          title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+          height: 1000,
+          width: 1000,
+          xaxis: {zeroline:false, showticklabels:false,
+                                 showgrid: false, range: [-1, 1]},
+          yaxis: {zeroline:false, showticklabels:false,
+                                 showgrid: false, range: [-1, 1]}
+        };
+
+        Plotly.newPlot("gaugeChart", data, layout);
+        });
+
 
 };
 
@@ -86,7 +158,7 @@ function bubbleChart(otus_ids,sample_vals) {
 			text: otuLabels,
 			mode: 'markers',
 			marker: {
-				size: sample_vals.map(d=>d+10),
+			size: sample_vals.map(d=>d+10),
 				color: sample_vals,
                     		colorscale: "Earth"
 			}
@@ -108,6 +180,7 @@ function bubbleChart(otus_ids,sample_vals) {
 
 	});
 };
+
 
 populateDropdown();
 optionChanged("BB_940");
