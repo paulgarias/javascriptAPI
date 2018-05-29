633f8a3f19776ba334985f5f126c5cdfccb4515c
Paul G. Arias <paul.g.arias@gmail.com>
2018-05-29 16:26:46 -0400

    Added additional template fixing.

--- static/js/bbbdd.js
+++ static/js/bbbdd.js
@@ -13,6 +13,53 @@ function populateDropdown() {
 	});
 }
 
+function runStart() {
+	var sampleID = "BB_940";
+	//alert("Selected: "+sampleID);
+	//fetch data for the newly selected sample
+	//console.log("http://127.0.0.1:5000/metadata/"+sampleID);
+
+	var selectmd = document.getElementById("mdbox");
+	if (selectmd.hasChildNodes()) {
+		var myNode = document.getElementById("mdbox");
+		while (myNode.firstChild) {
+			myNode.removeChild(myNode.firstChild);
+		};
+	};
+
+	Plotly.d3.json("http://127.0.0.1:5000/metadata/"+sampleID, function(error, response) {
+		//console.log(response);
+		for (key in response) {
+			var h5md = document.createElement("h5");
+			h5md.innerText = key + " : "+response[key];
+			selectmd.appendChild(h5md);
+		};
+	});
+
+	Plotly.d3.json("http://127.0.0.1:5000/samples/"+sampleID, function(error, response) {
+		if(error) console.error;
+		console.log(response);
+		var otus_ids = response.otu_ids;
+		var sample_vals = response.sample_values;
+		console.log(otus_ids.slice(0,10));
+		console.log(sample_vals.slice(0,10));
+		pieChart(otus_ids.slice(0,10),sample_vals.slice(0,10));
+		bubbleChart(otus_ids.slice(0,10), sample_vals.slice(0,10));
+	});
+
+        Plotly.d3.json("http://127.0.0.1:5000/wfreq/"+sampleID, function(error,response) {
+        	if(error) console.log(error);
+
+        	var level;
+        	if (response) {
+        		level = response;
+        	} else {
+        		level = 0;
+        	}
+		gaugeChart(level);
+        });
+};
+
 function optionChanged(sampleID) {
 	//alert("Selected: "+sampleID);
 	//fetch data for the newly selected sample
@@ -42,24 +89,120 @@ function optionChanged(sampleID) {
 		var sample_vals = response.sample_values;
 		console.log(otus_ids.slice(0,10));
 		console.log(sample_vals.slice(0,10));
-		pieChart(otus_ids.slice(0,10),sample_vals.slice(0,10));
-		bubbleChart(otus_ids.slice(0,10), sample_vals.slice(0,10));
+		updatePieChart(otus_ids.slice(0,10),sample_vals.slice(0,10));
+		updateBubbleChart(otus_ids.slice(0,10), sample_vals.slice(0,10));
 	});
 	
-        // Enter a speed between 0 and 180
         Plotly.d3.json("http://127.0.0.1:5000/wfreq/"+sampleID, function(error,response) {
-        if(error) console.log(error);
+        	if(error) console.log(error);
 
-        var level;
-        if (response) {
-                level = response
-        } else {
-                level = 0;
-        }
+        	var level;
+        	if (response) {
+        	        level = response
+        	} else {
+        	        level = 0;
+        	};
 
+		gaugeChart(level);
+        });
+};
+
+function pieChart(otus_ids,sample_vals) {
+	Plotly.d3.json("http://127.0.0.1:5000/otu", function(error, response) {
+		var otuLabels=[];
+		for (var i=0; i < otus_ids.length; i++) {
+			otuLabels.push(response[otus_ids[i]]);
+		};
+		console.log(otuLabels);
+		
+		var data = [{
+			values: sample_vals,
+			labels: otus_ids,
+			text: otuLabels,
+			textinfo: 'none',
+			type: 'pie'
+		}];
+		
+		var layout = {
+			height: 400,
+			width: innerWidth*0.6
+		};
+		
+		Plotly.newPlot("pieChart", data, layout);
+	});
+};
+
+function updatePieChart(otus_ids,sample_vals) {
+	var PIE = document.getElementById("pieChart");
+        Plotly.d3.json("http://127.0.0.1:5000/otu", function(error, response) {
+                var otuLabels=[];
+                for (var i=0; i < otus_ids.length; i++) {
+                        otuLabels.push(response[otus_ids[i]]);
+                };
+                console.log(otuLabels);
+
+                Plotly.restyle(PIE, "values", [sample_vals]);
+                Plotly.restyle(PIE, "labels", [otus_ids]);
+        });
+};
+
+
+function bubbleChart(otus_ids,sample_vals) {
+	Plotly.d3.json("http://127.0.0.1:5000/otu", function(error, response) {
+		var otuLabels=[];
+                for (var i=0; i < otus_ids.length; i++) {
+                        otuLabels.push(response[otus_ids[i]]);
+                };
+
+		var trace1 = {
+			x: otus_ids,
+			y: sample_vals,
+			text: otuLabels,
+			mode: 'markers',
+			marker: {
+				size: sample_vals.map(d=>d+10),
+				color: sample_vals,
+                    		colorscale: "Earth"
+			}
+		};
+		var data = [trace1];
+
+		var layout = {
+			showlegend: false,
+			xaxis: {
+				title:"OTU IDS"
+			},
+			yaxis: {
+				title: "Sample values"
+			},
+			height: 400,
+			width: innerWidth
+		};
+		Plotly.newPlot("scatterChart",data,layout);
+
+	});
+};
+
+function updateBubbleChart(otus_ids,sample_vals) {
+	var BUBBLE = document.getElementById("scatterChart");
+	Plotly.d3.json("http://127.0.0.1:5000/otu", function(error, response) {
+		var otuLabels=[];
+                for (var i=0; i < otus_ids.length; i++) {
+                        otuLabels.push(response[otus_ids[i]]);
+                };
+
+		Plotly.restyle(BUBBLE,"x",[otus_ids]);
+		Plotly.restyle(BUBBLE,"y",[sample_vals]);
+		Plotly.restyle(BUBBLE,"text",[otuLabels]);
+		Plotly.restyle(BUBBLE,"marker.size",[sample_vals.map(d=>d+10)]);
+		Plotly.restyle(BUBBLE,"marker.color",[sample_vals]);
+	});
+};
+
+function gaugeChart(levelVal) {
         // Trig to calc meter point
-        var degrees = 180 - (180./13.)*(level+1),
-                 radius = .5;
+        var degrees = 180 - (180./13.)*(levelVal+1);
+        var radius = .5;
         var radians = degrees * Math.PI / 180;
         var x = radius * Math.cos(radians);
         var y = radius * Math.sin(radians);
@@ -78,7 +221,7 @@ function optionChanged(sampleID) {
                 marker: {size: 28, color:'850000'},
                 showlegend: false,
                 name: 'wash freq.',
-                text: level,
+                text: levelVal,
                 hoverinfo: 'text+name'},
           { values: [degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel/numLevels, degreesLevel],
           rotation: 90,
@@ -115,72 +258,8 @@ function optionChanged(sampleID) {
         };
 
         Plotly.newPlot("gaugeChart", data, layout);
-        });
-
 
 };
-
-function pieChart(otus_ids,sample_vals) {
-	Plotly.d3.json("http://127.0.0.1:5000/otu", function(error, response) {
-		var otuLabels=[];
-		for (var i=0; i < otus_ids.length; i++) {
-			otuLabels.push(response[otus_ids[i]]);
-		};
-		console.log(otuLabels);
-		
-		var data = [{
-			values: sample_vals,
-			labels: otus_ids,
-			text: otuLabels,
-			textinfo: 'none',
-			type: 'pie'
-		}];
-		
-		var layout = {
-			height: 400,
-			width: innerWidth*0.6
-		};
-		
-		Plotly.newPlot("pieChart", data, layout);
-	});
-};
-
-function bubbleChart(otus_ids,sample_vals) {
-	Plotly.d3.json("http://127.0.0.1:5000/otu", function(error, response) {
-		var otuLabels=[];
-                for (var i=0; i < otus_ids.length; i++) {
-                        otuLabels.push(response[otus_ids[i]]);
-                };
-
-		var trace1 = {
-			x: otus_ids,
-			y: sample_vals,
-			text: otuLabels,
-			mode: 'markers',
-			marker: {
-			size: sample_vals.map(d=>d+10),
-				color: sample_vals,
-                    		colorscale: "Earth"
-			}
-		};
-		var data = [trace1];
-
-		var layout = {
-			showlegend: false,
-			xaxis: {
-				title:"OTU IDS"
-			},
-			yaxis: {
-				title: "Sample values"
-			},
-			height: 400,
-			width: innerWidth
-		};
-		Plotly.newPlot("scatterChart",data,layout);
-
-	});
-};
-
 
 populateDropdown();
-optionChanged("BB_940");
+runStart();
